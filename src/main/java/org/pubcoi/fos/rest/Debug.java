32895f26c8b0fd8b43ea3b3100613cdbc157c133
Ronald MacDonald <ronald@rmacd.com>
2021-02-11 05:20:34 +0000

    update: fix types for sample notices

--- src/main/java/org/pubcoi/fos/rest/Debug.java
+++ src/main/java/org/pubcoi/fos/rest/Debug.java
@@ -1,15 +1,21 @@
 package org.pubcoi.fos.rest;
 
 import org.pubcoi.fos.dao.NoticesRepo;
+import org.pubcoi.fos.models.cf.ArrayOfFullNotice;
 import org.pubcoi.fos.models.cf.FullNotice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class Debug {
-
+    private static final Logger logger = LoggerFactory.getLogger(Debug.class);
     NoticesRepo noticesRepo;
 
     public Debug(NoticesRepo noticesRepo) {
@@ -19,6 +25,15 @@ public class Debug {
     @GetMapping(value = "/api/notices")
     public List<FullNotice> getNotices() {
         return noticesRepo.findAll();
+    }
+
+    @PutMapping(value = "/api/notices", consumes = {MediaType.APPLICATION_XML_VALUE})
+    public String putNotices(@RequestBody ArrayOfFullNotice array) {
+        for (FullNotice fullNotice : array.getFullNotice()) {
+            logger.info("Inserting: notice " + fullNotice.getId());
+            noticesRepo.save(fullNotice);
+        }
+        return "ok";
     }
 
 }
